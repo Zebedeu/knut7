b8057eb393ca660b93e5c610745ac13d9df5d6ef
Zebedeu <marciozebedeu@gmail.com>
2018-12-28 17:49:22 +0100

    update

--- Config/Config.php
+++ Config/Config.php
@@ -1,16 +1,16 @@
 <?php
 
 /**
- * KNUT7 K7F (http://framework.artphoweb.com/)
- * KNUT7 K7F (tm) : Rapid Development Framework (http://framework.artphoweb.com/)
+ * KNUT7 K7F (http://knut7.com)
+ * KNUT7 K7F (tm) : Rapid Development Framework (http://knut7.com)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @link      http://github.com/zebedeu/artphoweb for the canonical source repository
- * @copyright (c) 2015.  KNUT7  Software Technologies AO Inc. (http://www.artphoweb.com)
- * @license   http://framework.artphoweb.com/license/new-bsd New BSD License
+ * @link      http://github.com/knut7/knut7 for the canonical source repository
+ * @copyright (c) 2015.  KNUT7  Software Technologies AO Inc. (http://knut7.com)
+ * @license   http://knut7.com/license/new-bsd New BSD License
  * @author    Marcio Zebedeu - artphoweb@artphoweb.com
  * @version   1.0.2
  */
@@ -18,15 +18,20 @@
 
 
 
-$MY_PROJECT_NAME = "App";
-
+if(isset($_GET['demo'])) {
+    $demo['demo'] = $_GET['demo'];
+    $MY_PROJECT_NAME = $demo['demo'];
+} else {
+    $MY_PROJECT_NAME = 'Ap';
+}
+$HEADER_TITLE ="";
 $code = 'en';
 
 /**
  *
  */
 define('TYPE', 'PDO'); // Insert ou type database Drive hire. \\ PDO, MYSQLI, POSTGROUL and SQLite
-define('DB_PORT', 3306);
+define('DB_PORT', 8889);
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * KNUT7 K7F (http://framework.artphoweb.com/)
- * KNUT7 K7F (tm) : Rapid Development Framework (http://framework.artphoweb.com/)
+ * KNUT7 K7F (http://knut7.com)
+ * KNUT7 K7F (tm) : Rapid Development Framework (http://knut7.com)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @link      http://github.com/zebedeu/artphoweb for the canonical source repository
- * @copyright (c) 2015.  KNUT7  Software Technologies AO Inc. (http://www.artphoweb.com)
- * @license   http://framework.artphoweb.com/license/new-bsd New BSD License
+ * @link      http://github.com/knut7/knut7 for the canonical source repository
+ * @copyright (c) 2015.  KNUT7  Software Technologies AO Inc. (http://knut7.com)
+ * @license   http://knut7.com/license/new-bsd New BSD License
  * @author    Marcio Zebedeu - artphoweb@artphoweb.com
  * @version   1.0.2
  */
@@ -17,9 +17,10 @@
 
 use Ballybran\Helpers\Routing\Routes;
 
-require_once "vendor/knut7/framework/src/Ballybran/Config/autoload.php";
+
 require 'vendor/knut7/framework/src/Ballybran/Config/Config.php';
 require 'vendor/autoload.php';
+require "vendor/knut7/framework/src/Ballybran/Config/autoload.php";
 
 /**
  *
@@ -29,16 +30,33 @@ require 'vendor/autoload.php';
 
 ini_set("display_errors", 1);
 
+
 $registry = \Ballybran\Database\RegistryDatabase::getInstance();
 $registry->set("PDO",
 
                 new \Ballybran\Database\Drives\AbstractDatabasePDO
-                (
+                    (
                     include __DIR__.'/Config/Database/Config.php'
-                )
+                    )
+                );
+$registry->set("MYSQL",
+
+    new \Ballybran\Database\Drives\AbstractDatabaseMysqli()
 );
+$registry->set("POSTGL",
+
+    new \Ballybran\Database\Drives\AbstractDatabasePDO
+    (
+        include __DIR__.'/Config/Database/Config.php'
+    )
+);
+Routes::route();
 
 
-    Routes::route();
+//Map::add('User/', null, function() {
+//})->exe();
+//
+//$c = Map::add('Index/', null, function() {
+//})->exe();
 
 exit;
